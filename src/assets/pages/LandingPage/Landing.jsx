514bc770c1f075423d7db46ec2bc8a89746153c8
SebasMarquez <sebastian.marquez@correo.unimet.edu.ve>
2023-03-11 10:30:55 -0400

    primer commit

---
+++ src/assets/pages/LandingPage/Landing.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from './Landing.module.css'
+
+function Landing(){
+    return(
+        <div className="d-flex justify-content-evenly flex-wrap">
+            <h1>Hola xd</h1>
+        </div>
+    )
+}
+
+export default Landing
---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {Routes,Route, BrowserRouter} from 'react-router-dom';
+import App from './App'
+import { Layout } from './assets/components/Layout/Layout'
+import { HOME_URL,LOGIN_URL,REGISTER_URL } from './assets/constants/urls'
+import { Login } from './assets/pages/LoginPage/Login';
+import { Register } from './assets/pages/RegisterPage/Register';
+import './index.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route path = {HOME_URL} element={<App/>}/>
+          <Route path = {LOGIN_URL} element={<Login />}/>
+          <Route path={REGISTER_URL} element={<Register />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
