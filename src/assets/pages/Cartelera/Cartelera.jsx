64e8a3137e1101d2c275a958ad3abf89d488ff95
danieljdangelo <danieljdangelo97@gmail.com>
2023-03-11 20:58:59 -0400

    Landing y cartelera

---
+++ src/assets/pages/Cartelera/Cartelera.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import styles from "./Cartelera.module.css"
+import { CARTELERA_URL } from "../../constants/urls"
+
+function Cartelera(){
+    return(
+        <div>
+
+        </div>
+    )
+
+}
+export default Cartelera
